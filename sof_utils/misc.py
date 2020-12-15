149bdab1e797f86f9d7a654f8fd255ae399fec5d
Stefan Reinhold <stefan@sreinhold.com>
2020-12-15 14:22:03 +0100

    Return SOF ID as integer instead of str

--- sof_utils/misc.py
+++ sof_utils/misc.py
@@ -2,4 +2,4 @@ def id_and_visit_from_filename(filename: str) -> str:
     import re
     pattern="SF([0-9]+)V([0-9]+)H.dcm"
     match = re.match(pattern, filename)
-    return match.groups()[0], int(match.groups()[1])
+    return int(match.groups()[0]), int(match.groups()[1])
