bbf35c6ade8eaf3b7aa877c5fa573dbed3dac449
Stefan Reinhold <stefan@sreinhold.com>
2021-02-15 17:27:20 +0100

    Update changelog script

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -eou pipefail
+
 VERSION=${GITHUB_REF#refs/tags/}
+echo "VERSION=${VERSION}"
 sed -n "/## ${VERSION}/,/## [0-9]*\.[0-9]*\.[0-9]*/ p" CHANGELOG.md | sed '$d'
 
