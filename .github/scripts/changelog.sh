15d8f049c282872de37ccc10d0cbfe4cf2a45165
Stefan Reinhold <stefan@sreinhold.com>
2021-02-16 20:17:55 +0100

    Fix changelog generation script

--- .github/scripts/changelog.sh
+++ .github/scripts/changelog.sh
@@ -3,9 +3,8 @@
 set -eou pipefail
 
 VERSION=${GITHUB_REF#refs/tags/}
-CHANGELOG=$(sed -n "/## ${VERSION}/,/## [0-9]*\.[0-9]*\.[0-9]*/ p" CHANGELOG.md | sed '$d')
+CHANGELOG=$(sed -n "/## ${VERSION}/,/## v?[0-9]*\.[0-9]*\.[0-9]*/ p" CHANGELOG.md | sed '$d')
 CHANGELOG="${CHANGELOG//'%'/'%25'}"
 CHANGELOG="${CHANGELOG//$'\n'/'%0A'}"
 CHANGELOG="${CHANGELOG//$'\r'/'%0D'}"
 echo "$CHANGELOG"
-
